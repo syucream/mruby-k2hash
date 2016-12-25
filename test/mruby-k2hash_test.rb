c8bfc5cbfcce3bf589329eb8d1d5f44d45fa8887
Ryo Okubo <syucream1031@gmail.com>
2016-12-25 20:47:25 +0900

    Add unit test

---
+++ test/mruby-k2hash_test.rb
@@ -0,0 +1,69 @@
+class MrubyK2hashTest < MTest::Unit::TestCase
+  K2HASH_FILENAME = '/tmp/mtest.k2hash'
+
+  def test_closed
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.close
+
+    assert_true k2hash.closed?
+  end
+
+  def test_delete
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.store('key1', 'value1')
+    k2hash.delete('key1')
+
+    assert_nil k2hash.fetch('key1')
+  end
+
+  def test_fetch_store
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.store('key1', 'value1')
+
+    assert_equal k2hash.fetch('key1'), 'value1'
+    assert_nil k2hash.fetch('key100')
+
+    k2hash['key2'] = 'value2'
+
+    assert_equal k2hash['key2'], 'value2'
+    assert_nil k2hash['key200']
+  end
+
+  def test_has_key
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.store('key1', 'value1')
+
+    assert_true k2hash.has_key?('key1')
+    assert_true k2hash.key?('key1')
+    assert_true k2hash.include?('key1')
+    assert_true k2hash.member?('key1')
+
+    k2hash.delete('key1')
+
+    assert_false k2hash.has_key?('key1')
+    assert_false k2hash.key?('key1')
+    assert_false k2hash.include?('key1')
+    assert_false k2hash.member?('key1')
+  end
+
+  def test_keys
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.store('key1', 'value1')
+    k2hash.store('key2', 'value2')
+    k2hash.store('key3', 'value3')
+
+    assert_equal k2hash.keys, ['key1', 'key2', 'key3']
+  end
+
+  def test_values
+    k2hash = K2Hash.new(K2HASH_FILENAME, 0666, K2Hash::NEWDB)
+    k2hash.store('key1', 'value1')
+    k2hash.store('key2', 'value2')
+    k2hash.store('key3', 'value3')
+
+    assert_equal k2hash.values, ['value1', 'value2', 'value3']
+  end
+
+end
+
+MTest::Unit.new.run
