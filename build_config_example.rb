ec1b22bafce81d4be87cd9f563d4cab538c23f8e
Ryo Okubo <syucream1031@gmail.com>
2017-01-03 14:55:16 +0900

    Fix build_config

--- build_config_example.rb
+++ build_config_example.rb
@@ -2,10 +2,10 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 
   enable_debug
+  # k2hash requires C++ so it enables cxxabi
+  enable_cxx_abi
 
   conf.gembox 'default'
 
-  # k2hash requires C++ so it enables cxxabi
-  enable_cxx_abi
   conf.gem :github => 'syucream/mruby-k2hash'
 end
